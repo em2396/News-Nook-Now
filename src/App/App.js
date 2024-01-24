76acaf036bba2fb68433f96bf61908868649d3a9
Emalee Poellot <emalee.poellot@gmail.com>
2024-01-24 16:53:53 -0700

    feat/Add background image and basic styling

--- src/App/App.js
+++ src/App/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import NewsStories from '../NewsStories/NewsStories';
 import topHeadlines from '../sampleData/topHeadlines';
 import SingleNewsDetails from '../SingleNewsDetails/SingleNewDetails';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 // import { getStories } from '../apiCalls/apiCall';
 
 export default function App() {
@@ -31,20 +31,24 @@ export default function App() {
   }
 
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <>
-            <div className="App">
-              <header>News Nook Now</header>
-              <NewsStories stories={stories} displayNewsStory={displayNewsStory}/>
-            </div>
-          </>
-          }
-      />
-      <Route path="/article/:id" element={<SingleNewsDetails selectedNewsStory={selectedNewsStory} displayHomePage={displayHomePage}/>} />
-    </Routes>
+    <main className="main-container">
+      <header>
+          <Link to="/" style={{ textDecoration: 'none', color: 'inherit'}}>News Nook Now</Link>
+      </header>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="App">
+                <NewsStories className="news-stories" stories={stories} displayNewsStory={displayNewsStory}/>
+              </div>
+            </>
+            }
+            />
+        <Route path="/article/:id" element={<SingleNewsDetails selectedNewsStory={selectedNewsStory} displayHomePage={displayHomePage}/>} />
+      </Routes>
+    </main>
   );
 }
 
